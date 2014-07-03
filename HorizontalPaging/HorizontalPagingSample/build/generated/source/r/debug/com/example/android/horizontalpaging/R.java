8c240e15511510c5ea3db007ba89535c4bad10c5
Yang-Jae Ahn <yangjae.ahn@gmail.com>
2014-07-03 20:54:09 +0900

    gradle setting modification
    gradle setting modification

--- HorizontalPaging/HorizontalPagingSample/build/generated/source/r/debug/com/example/android/horizontalpaging/R.java
+++ HorizontalPaging/HorizontalPagingSample/build/generated/source/r/debug/com/example/android/horizontalpaging/R.java
@@ -9,25 +9,55 @@ package com.example.android.horizontalpaging;
 
 public final class R {
     public static final class attr {
+        /**  Background color for CardView. 
+         <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cardBackgroundColor=0x7f010000;
+        /**  Corner radius for CardView. 
+         <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cardCornerRadius=0x7f010001;
+    }
+    public static final class color {
+        public static final int cardview_dark_background=0x7f040000;
+        public static final int cardview_light_background=0x7f040001;
+        public static final int cardview_shadow_end_color=0x7f040002;
+        public static final int cardview_shadow_start_color=0x7f040003;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int horizontal_page_margin=0x7f040002;
-        public static final int margin_huge=0x7f040003;
-        public static final int margin_large=0x7f040004;
-        public static final int margin_medium=0x7f040005;
-        public static final int margin_small=0x7f040006;
-        public static final int margin_tiny=0x7f040007;
-        public static final int vertical_page_margin=0x7f040008;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int cardview_default_radius=0x7f050002;
+        public static final int cardview_elevation=0x7f050003;
+        public static final int cardview_shadow_size=0x7f050004;
+        public static final int horizontal_page_margin=0x7f050005;
+        public static final int margin_huge=0x7f050006;
+        public static final int margin_large=0x7f050007;
+        public static final int margin_medium=0x7f050008;
+        public static final int margin_small=0x7f050009;
+        public static final int margin_tiny=0x7f05000a;
+        public static final int vertical_page_margin=0x7f05000b;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int tile=0x7f020001;
     }
     public static final class id {
-        public static final int pager=0x7f070001;
-        public static final int section_label=0x7f070000;
+        public static final int pager=0x7f080001;
+        public static final int section_label=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,19 +65,74 @@ public final class R {
         public static final int sample_main=0x7f030002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int intro_message=0x7f050002;
-        public static final int title_section1=0x7f050003;
-        public static final int title_section2=0x7f050004;
-        public static final int title_section3=0x7f050005;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int intro_message=0x7f060002;
+        public static final int title_section1=0x7f060003;
+        public static final int title_section2=0x7f060004;
+        public static final int title_section3=0x7f060005;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
-        public static final int Theme_Base=0x7f060001;
-        public static final int Theme_Sample=0x7f060002;
-        public static final int Widget=0x7f060003;
-        public static final int Widget_SampleMessage=0x7f060004;
-        public static final int Widget_SampleMessageTile=0x7f060005;
+        public static final int AppTheme=0x7f070000;
+        public static final int CardView=0x7f070001;
+        public static final int CardView_Dark=0x7f070002;
+        public static final int CardView_Light=0x7f070003;
+        public static final int Theme_Base=0x7f070004;
+        public static final int Theme_Sample=0x7f070005;
+        public static final int Widget=0x7f070006;
+        public static final int Widget_SampleMessage=0x7f070007;
+        public static final int Widget_SampleMessageTile=0x7f070008;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CardView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CardView_cardBackgroundColor com.example.android.horizontalpaging:cardBackgroundColor}</code></td><td> Background color for CardView.</td></tr>
+           <tr><td><code>{@link #CardView_cardCornerRadius com.example.android.horizontalpaging:cardCornerRadius}</code></td><td> Corner radius for CardView.</td></tr>
+           </table>
+           @see #CardView_cardBackgroundColor
+           @see #CardView_cardCornerRadius
+         */
+        public static final int[] CardView = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>
+          @attr description
+           Background color for CardView. 
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          <p>This is a private symbol.
+          @attr name com.example.android.horizontalpaging:cardBackgroundColor
+        */
+        public static final int CardView_cardBackgroundColor = 0;
+        /**
+          <p>
+          @attr description
+           Corner radius for CardView. 
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          <p>This is a private symbol.
+          @attr name com.example.android.horizontalpaging:cardCornerRadius
+        */
+        public static final int CardView_cardCornerRadius = 1;
+    };
 }
